08e482c263cf076f1cebb4510e5117ecff5554ab
navfal2010 <navfal808@gmail.com>
2024-06-02 20:12:01 +0500

    done

---
+++ main.js
@@ -0,0 +1,30 @@
+let wrapper = document.querySelector('.mySwiper')
+let first = document.querySelector('.first_card')
+let second = document.querySelector('.second_card')
+let third = document.querySelector('.third_card')
+let fourth = document.querySelector('.fourth_card')
+
+first.innerHTML = `
+<p class='number'>01</p>
+<p class='p-lorem'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni culpa soluta, dolorum at illum deleniti sint doloribus iure provident nostrum!</p>
+<img class='photo' src="https://media.istockphoto.com/id/1069539210/photo/fantastic-autumn-sunset-of-hintersee-lake.jpg?s=612x612&w=0&k=20&c=oqKJzUgnjNQi-nSJpAxouNli_Xl6nY7KwLBjArXr_GE=" alt="">
+`
+
+second.innerHTML = `
+<p class='number'>02</p>
+<p class='p-lorem'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni culpa soluta, dolorum at illum deleniti sint doloribus iure provident nostrum!</p>
+<img class='photo' src="https://e0.pxfuel.com/wallpapers/434/453/desktop-wallpaper-mac-nature-for-your-mobile-tablet-explore-nature-background-for-mac-for-mac-s-for-mac-horizontal-nature-thumbnail.jpg" alt="">
+`
+
+third.innerHTML = `
+<p class='number'>03</p>
+<p class='p-lorem'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni culpa soluta, dolorum at illum deleniti sint doloribus iure provident nostrum!</p>
+<img class='photo' src="https://wallpapercrafter.com/desktop2/831894-green-grass-grass-field-body-of-water-white-clouds.jpg" alt="">
+`
+
+fourth.innerHTML = `
+<p class='number'>04</p>
+<p class='p-lorem'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni culpa soluta, dolorum at illum deleniti sint doloribus iure provident nostrum!</p>
+<img class='photo' src="https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/3/autumn-illuminated-steve-henderson.jpg" alt="">
+`
+
